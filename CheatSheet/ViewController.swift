52c706403f86a9ae886249347d5b6e65b7e676c2
TimurShafigullin <timur.shafigullin@flatstack.com>
2019-01-30 11:04:03 +0300

    Initial Commit

---
+++ CheatSheet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheatSheet
+//
+//  Created by Timur Shafigullin on 30/01/2019.
+//  Copyright © 2019 Timur Shafigullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
