cd5507270e55a211bc89d71f5f8401c88062a60b
TimurShafigullin <timur.shafigullin@flatstack.com>
2019-01-30 11:41:07 +0300

    Setup project

---
+++ CheatSheet/ViewControllers/Main/CheatSheetsTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CheatSheetsTableViewController.swift
+//  CheatSheet
+//
+//  Created by Timur Shafigullin on 30/01/2019.
+//  Copyright © 2019 Timur Shafigullin. All rights reserved.
+//
+
+import UIKit
+
+class CheatSheetsTableViewController: UIViewController {
+    
+    @IBOutlet fileprivate weak var tableView: UITableView!
+    
+}
